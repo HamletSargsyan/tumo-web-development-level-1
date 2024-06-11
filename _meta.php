2f9b8630ca6d91ec8abf9ca9fe3db4f7010c2214
Hamlet <115415995+HamletSargsyan@users.noreply.github.com>
2024-06-11 19:20:50 +0400

    Add files via upload

---
+++ _meta.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$pages = [
+    "home" => [
+        "url" => "index.php"
+    ],
+    "loop" => [
+        "url" => "loop.php"
+    ],
+    // "task" => [
+    //     "url" => "task.php"
+    // ],
+    "single" => [
+        "url" => "single.php"
+    ],
+    "product" => [
+        "url" => "product.php"
+    ]
+];
+
+$posts = [
+
+]
+
+
+?>
--- single.php
+++ single.php
@@ -1,33 +1,39 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Blog</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
+</head>
+
+<body>
     <?php
     include("_navbar.php");
     include("_meta.php");
-    $email = "John@gmail.com";
-    $new_password_input = "";
-
-
-    if ($email != "John@gmail.com") {
-        echo "<div class='alert alert-success' role='alert'>
-        Email not found!
-        </div>";
-    } else {
-        $new_password_input = "5487494";
-        echo "<div class='alert alert-success' role='alert'>
-        OK
-        </div>";
-    }
     ?>
+
+    <div class="container">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://via.placeholder.com/600/9c184f" class="d-block w-50" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://via.placeholder.com/600/fdf73e" class="d-block w-50" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://via.placeholder.com/600/f9cee5" class="d-block w-50" alt="...">
+    </div>
+  </div>
+</div>
+    </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
