23d13997bcc063d9e442038b6a4b5481644151b3
Hamlet <hamlets849@gmail.com>
2024-06-20 14:57:38 +0400

    правки

--- src/post.php
+++ src/post.php
@@ -2,23 +2,17 @@
 include("_meta.php");
 include("_navbar.php");
 
-// Получаем ID поста из URL
 $postId = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 ?>
 
-
-
-
 <div class="container">
     <div id="post" class="mt-4">
-
     </div>
 
     <hr>
     <h3>Comments</h3>
 
     <div id="comments">
-
     </div>
 </div>
 
@@ -45,9 +39,6 @@ fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
             });
     });
 
-
-
-
 fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
     .then(response => response.json())
     .then(comments => {
@@ -55,22 +46,22 @@ fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
         commentsContainer.innerHTML = '';
 
         comments.forEach(comment => {
-            getUser(comment.id).then(user => {
+                var avatar_url = `https://api.dicebear.com/9.x/identicon/svg?seed=${comment.email}`;
                 commentsContainer.innerHTML += `
-                  <div class="comment card mt-3">
-                      <div class="card-body">
-                          <div class="d-flex align-items-center">
-                              <img src="${user.picture.thumbnail}" class="rounded-circle" style="margin-right: 10px;" width="50" height="50" alt="Avatar">
-                              <div>
-                                  <h5 class="card-title">${user.name.title} ${user.name.first} ${user.name.last}</h5>
-                                  <h6 class="card-subtitle mb-2 text-muted">@${user.login.username}</h6>
-                              </div>
-                          </div>
-                          <p class="card-text">${comment.body}</p>
-                      </div>
-                  </div>
-              `;
+                <div class="card mt-3" style="cursor: default !important;">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <img src="${avatar_url}" class="rounded-circle" style="margin-right: 10px;" width="50" height="50" alt="avatar">
+                            <div>
+                                <h5 class="card-title">${comment.name}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">${comment.email}</h6>
+                            </div>
+                        </div>
+                        <p class="card-text">${comment.body}</p>
+                    </div>
+                </div>
+            `;
             });
         });
-    });
+
 </script>
--- src/profile.php
+++ src/profile.php
@@ -1,8 +1,34 @@
 <?php
 include("_meta.php");
 include("_navbar.php");
+
+$userId = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 ?>
 
 <div class="container">
+    <div class="text-center" id="profile">
 
+    </div>
 </div>
+
+
+<script>
+const userId = <?php echo $userId; ?>;
+
+fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    .then(response => response.json())
+    .then(user => {
+        const profileElement = document.getElementById('profile');
+        const avatarUrl = `https://api.dicebear.com/9.x/identicon/svg?seed=${encodeURIComponent(user.username)}`;
+        profileElement.innerHTML = `
+            <img src="${avatarUrl}" class="mx-auto d-block" height=200px width=200px alt="avatar">
+            <br>
+            <h2>${user.name}</h2>
+            <p>@${user.username}</p>
+            <p><i class="bi bi-envelope"></i> ${user.email}</p>
+            <p><i class="bi bi-telephone"></i> ${user.phone}</p>
+            <p><i class="bi bi-globe"></i> <a href="www.${user.website}" target=”_blank”>${user.website}</a></p>
+        `
+    });
+
+</script>
